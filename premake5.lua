300c25a6e3b70dacea6e858d415714f1e79a7caf
Rynhho <rynhho@gmail.com>
2021-01-04 22:45:18 +0100

    First commit

---
+++ premake5.lua
@@ -0,0 +1,65 @@
+workspace "Yolo!"
+    architecture "x64"
+    
+    configurations
+    {
+        "Debug",
+        "DebugOptOn",
+        "Release"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.architecture}-%{cfg.system}"
+
+IncludeDir = {}
+
+-- IncludeDir["HeaderLibs"]    = "Ocean/Source/Thirdparty/Headers"
+
+project "Yolo!"
+    location "Yolo!"
+    kind "ConsoleApp"
+    staticruntime "On"
+
+    language "C++"
+    cppdialect "C++17"
+
+    floatingpoint "Fast"
+
+    warnings "Extra"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-obj/" .. outputdir .. "/%{prj.name}")
+
+    debugdir ("%{cfg.targetdir}")
+
+    files
+    {
+        "%{prj.name}/*.txt"
+    }
+
+    includedirs
+    {
+        "%{prj.name}/Source",
+
+        -- "%{IncludeDir.HeaderLibs}"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        defines "YOLO_MODE_DEBUG"
+        runtime "Debug"
+        symbols "On"
+        optimize "Off"
+
+    filter "configurations:DebugOptOn"
+        defines "YOLO_MODE_DEBUGOPTON"
+        runtime "Release"
+        symbols "On"
+        optimize "On"
+
+    filter "configurations:Release"
+        defines "YOO_MODE_RELEASE"
+        runtime "Release"
+        symbols "Off"
+        optimize "On"
