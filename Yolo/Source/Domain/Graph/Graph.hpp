221279bf3df48e0c410698c38615a038ce4cb549
Sylvain Lichau <sylvain.lichau@etu.u-bordeaux.fr>
2021-01-21 14:35:46 +0100

    vertexDegree, maxDegree and minDegree stocked

--- Yolo/Source/Domain/Graph/Graph.hpp
+++ Yolo/Source/Domain/Graph/Graph.hpp
@@ -9,9 +9,32 @@ namespace Yolo
     class Graph
     {
     public:
-        Graph(const std::vector<std::vector<Edge>>& adjacencyList) { mAdjacencyList = std::vector<std::vector<Edge>>(adjacencyList); }
+        Graph(const std::vector<std::vector<Edge>>& adjacencyList) { 
+            mAdjacencyList = std::vector<std::vector<Edge>>(adjacencyList); 
+            vertexDegree = std::vector<int>(mAdjacencyList.size());
+
+            maxDegree = mAdjacencyList[0].size();
+            minDegree = mAdjacencyList[0].size();
+
+            for(int i = 0; i<mAdjacencyList.size(); ++i){
+                vertexDegree[i] = mAdjacencyList[i].size();
+                if(vertexDegree[i] > maxDegree)
+                    maxDegree = vertexDegree[i];
+                if(vertexDegree[i] < minDegree)
+                    minDegree = vertexDegree[i];
+            }
+
+            
+        }
+
+        int getVertexDegree(int vertex){
+            return vertexDegree[vertex];
+        }
 
     private:
+        int maxDegree;
+        int minDegree;
         std::vector<std::vector<Edge>> mAdjacencyList;
+        std::vector<int> vertexDegree;
     };
 }
