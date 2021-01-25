f4ac781d4349dca96a310d0586f4628b2f0b3f20
Sylvain Lichau <sylvain.lichau@etu.u-bordeaux.fr>
2021-01-25 15:21:15 +0100

    criterion as parameter

--- Yolo/Source/Domain/Algorithm/Algorithm.hpp
+++ Yolo/Source/Domain/Algorithm/Algorithm.hpp
@@ -9,13 +9,14 @@ namespace Yolo
     class Algorithm
     {
     public:
-        Algorithm(const Graph& graph, int nbClasses) : mGraph(graph), mNbClasses(nbClasses) {}
+        Algorithm(const Graph& graph, int nbClasses, bool (*criterion)(std::vector<int>, int, int)) : mGraph(graph), mNbClasses(nbClasses), mCriterion(criterion) {}
 
         virtual Solution solve() = 0;
 
     protected:
         Graph mGraph;
-
+        
         const int mNbClasses;
+        bool (*mCriterion)(std::vector<int>, int, int);
     };
 }
