bab0f51af53d4657874eb23282cd89bf15e8a096
Sylvain Lichau <sylvain.lichau@etu.u-bordeaux.fr>
2021-01-25 18:07:08 +0100

    new sandbox

--- Yolo/Source/UserInterface/Sandbox.cpp
+++ Yolo/Source/UserInterface/Sandbox.cpp
@@ -1,17 +1,25 @@
 #include "Core/Core.hpp"
 
 #include "Technical/Repositories/GraphFileRepository.hpp"
+#include "Domain/Algorithm/ExplicitEnumerationAlgorithm.hpp"
+#include "Domain/Algorithm/Criterion.hpp"
 
 #include <iostream>
 
+
 int main()
 {
+    printf("start\n");
     Yolo::Core::initialize();
 
     Yolo::GraphFileRepository graphRepository;
 
     std::optional<Yolo::Graph> g = graphRepository.load("Instances/quatreSommets.txt");
-
+    if(g.has_value()){
+        Yolo::ExplicitEnumerationAlgorithm algo = Yolo::ExplicitEnumerationAlgorithm(g.value(), 2, Yolo::cEqual);
+        Yolo::Solution sol = algo.solve();
+        std::cout << "\nBest solution: " << sol.toString() << " cost: " << g.value().getSolutionCost(sol) << "\n";
+    }
     Yolo::Core::shutdown();
 
     return 0;
