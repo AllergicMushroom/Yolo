6394126f347c61f541a269cace065e574d9f74bf
Sylvain Lichau <sylvain.lichau@etu.u-bordeaux.fr>
2021-01-27 10:38:28 +0100

    solution initialise in constructor

--- Yolo/Source/Domain/Algorithm/ExplicitEnumerationAlgorithm.hpp
+++ Yolo/Source/Domain/Algorithm/ExplicitEnumerationAlgorithm.hpp
@@ -7,13 +7,13 @@ namespace Yolo
     class ExplicitEnumerationAlgorithm : public Algorithm
     {
     public:
-        ExplicitEnumerationAlgorithm(const Graph& graph, int nbClasses, bool (*criterion)(std::vector<int>, int, int)) : Algorithm(graph, nbClasses, criterion){}
+        ExplicitEnumerationAlgorithm(const Graph& graph, int nbClasses, bool (*criterion)(std::vector<int>, int, int)) : Algorithm(graph, nbClasses, criterion), mBest(Solution(mGraph.getNbVertices(), mNbClasses)){}
 
         virtual Solution solve() override;
         void enumerateFrom(Solution sol, int from);
         void compareBest(Solution sol);
 
     protected:
-        Solution mBest = Solution(mGraph.getNbVertices(), mNbClasses);
+        Solution mBest;
     };
 }
--- Yolo/Source/Domain/Algorithm/ImplicitEnumerationAlgorithm.hpp
+++ Yolo/Source/Domain/Algorithm/ImplicitEnumerationAlgorithm.hpp
@@ -7,13 +7,13 @@ namespace Yolo
     class ImplicitEnumerationAlgorithm : public Algorithm
     {
     public:
-        ImplicitEnumerationAlgorithm(const Graph& graph, int nbClasses, bool (*criterion)(std::vector<int>, int, int)) : Algorithm(graph, nbClasses, criterion){}
+        ImplicitEnumerationAlgorithm(const Graph& graph, int nbClasses, bool (*criterion)(std::vector<int>, int, int)) : Algorithm(graph, nbClasses, criterion), mBest(Solution(mGraph.getNbVertices(), mNbClasses)){}
 
         virtual Solution solve() override;
         void enumerateFrom(Solution sol, int from);
         void compareBest(Solution sol);
 
     private:
-        Solution mBest = Solution(mGraph.getNbVertices(), mNbClasses);
+        Solution mBest;
     };
 }
