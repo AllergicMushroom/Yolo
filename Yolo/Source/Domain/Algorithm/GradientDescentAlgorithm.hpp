16e71e3ed51f065279c8bd25a792659911639738
Sylvain Lichau <sylvain.lichau@etu.u-bordeaux.fr>
2021-02-04 11:19:28 +0100

    gradient descent

---
+++ Yolo/Source/Domain/Algorithm/GradientDescentAlgorithm.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Algorithm.hpp"
+
+namespace Yolo
+{
+    class GradientDescentAlgorithm : public Algorithm
+    {
+    public:
+        GradientDescentAlgorithm(const Graph& graph, int nbClasses, double epsilon, std::vector<Solution> (*neigh)(Solution sol), bool (*criterion)(std::vector<int>, int, int)) : Algorithm(graph, nbClasses, criterion), mActual(Solution(mGraph.getNbVertices(), mNbClasses)){
+            mNeigh = neigh;
+            mEpsilon = epsilon;
+        }
+
+        virtual Solution solve() override;
+        virtual Solution solve(Solution initialSolution);
+
+        Solution findBestNeighbour(Solution sol, bool real);
+        Solution generateValidSolution();
+
+    protected:
+        Solution mActual;
+        double mActualCost = 0;
+        double mEpsilon;
+        std::vector<Solution> (*mNeigh)(Solution sol);
+    };
+}
