2d581d2758a21c24abe278a74191ac2cd50c399e
Sylvain Lichau <sylvain.lichau@etu.u-bordeaux.fr>
2021-02-04 11:16:29 +0100

    new criterions (percentage and delta)

--- Yolo/Source/Domain/Algorithm/Criterion.cpp
+++ Yolo/Source/Domain/Algorithm/Criterion.cpp
@@ -1,5 +1,5 @@
 #include "Criterion.hpp"
-
+#include <cmath>
 namespace Yolo
 {
     bool cAll(std::vector<int> array, int nbVertices, int to){
@@ -11,9 +11,38 @@ namespace Yolo
             to = array.size();
         }
         int elementPerClass = nbVertices / array.size();
+        for (unsigned int i = 0; i < array.size(); ++i)
+        {
+            if(array[i] > elementPerClass){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+
+    bool cEqualDelta(std::vector<int> array, int nbVertices, int to, int delta){
+        if(to == -1){
+            to = array.size();
+        }
+        int elementPerClass = floor(nbVertices / array.size());
         for (int i = 0; i < to; ++i)
         {
-            if(array[i] > elementPerClass)
+            if(array[i] > elementPerClass + delta || array[i] < elementPerClass - delta)
+                return false;
+        }
+        
+        return true;
+    }
+
+    bool cEqualPercentage(std::vector<int> array, int nbVertices, int to, double percentage){
+        if(to == -1){
+            to = array.size();
+        }
+        int elementPerClass = floor(nbVertices / array.size());
+        for (int i = 0; i < to; ++i)
+        {
+            if(static_cast<double>(array[i]) > elementPerClass*(1+percentage) || static_cast<double>(array[i]) > elementPerClass*(1-percentage))
                 return false;
         }
         
